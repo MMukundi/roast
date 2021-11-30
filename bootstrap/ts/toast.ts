5613f589db1bf36590e2806247d458f2e843dd06
Marcel Mukundi <45983298+MMukundi@users.noreply.github.com>
2021-11-29 18:55:09 -0800

    Made 'toast not implemented' a warning

--- bootstrap/ts/toast.ts
+++ bootstrap/ts/toast.ts
@@ -2,13 +2,19 @@ import { SourceFile } from "./arguments"
 import { BufferedStyledLogger, ConsoleColor, StringStyler } from "./style"
 const errorStyler = new BufferedStyledLogger(process.stdout.write.bind(process.stdout))
 errorStyler.fg(ConsoleColor.Red)
+const warningStyler = new BufferedStyledLogger(process.stdout.write.bind(process.stdout))
+warningStyler.fg(ConsoleColor.Yellow)
 
 function toastError(errorString: string) {
 	errorStyler.styleLog(`toast {error}: ${errorString}\n`)
 	errorStyler.flush()
 }
+function toastWarning(errorString: string) {
+	warningStyler.styleLog(`toast {warn} : ${errorString}\n`)
+	warningStyler.flush()
+}
 
-toastError('toast not implemented')
+toastWarning('toast not implemented')
 if (!SourceFile) {
 	toastError('no source file provided')
 }
