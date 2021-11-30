1cc307bd94580007c88d5cf9e5dda3368105b0bc
Marcel Mukundi <45983298+MMukundi@users.noreply.github.com>
2021-11-30 07:48:32 -0800

    Added char utils

---
+++ bootstrap/ts/utils.ts
@@ -0,0 +1,38 @@
+const ZeroCode = '0'.charCodeAt(0)
+export function escapeChar(c: string): string {
+	switch (c) {
+		case "n":
+			return '\n' as string;
+		case "t":
+			return '\t' as string;
+		case "r":
+			return '\r' as string;
+		case '"':
+			return '"' as string;
+		default:
+			return c;
+	}
+}
+export function unescapeChar(c: string): string {
+	switch (c) {
+		case "\n":
+			return '\\n' as string;
+		case "\t":
+			return '\\t' as string;
+		case "\r":
+			return '\\r' as string;
+		case '"':
+			return '"' as string;
+		default:
+			return c;
+	}
+}
+export function digitCode(c: string): number {
+	return (c.charCodeAt(0) - ZeroCode)
+}
+export function isDigitCode(c: number): boolean {
+	return -1 < c && c < 10;
+}
+export function isWhitespace(c: string): boolean {
+	return c.trim().length === 0
+}
