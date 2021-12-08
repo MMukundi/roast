f52eb25e5581f57900b995e8732c71a1ff46de48
Marcel Mukundi <45983298+MMukundi@users.noreply.github.com>
2021-12-07 21:46:54 -0800

    Added basic command line argument parsing

--- bootstrap/ts/arguments.ts
+++ bootstrap/ts/arguments.ts
@@ -1,15 +1,51 @@
 import path from "path"
 
-// All of the arguments to the program (with the node command and the filename)
+/** All of the arguments to the program (with the node command and the filename) */
 const CommandLineArguments = process.argv
-// The arguments to the compiler
+/** The arguments to the compiler */
 export const CompilerArguments = CommandLineArguments.slice(2)
 
-// An 'input' is any argument that doesn't start with a dash
-export const Inputs = CompilerArguments.filter(arg => !arg.startsWith('-'))
+/** An 'input' is any argument that doesn't start with a dash, and isn't input to an option */
+export const Inputs: string[] = []
+//CompilerArguments.filter(arg => !arg.startsWith('-'))
 
-// The first input is assumed to be the source file
+
+export enum Options {
+	/** The file to output to */
+	OutputFile = "o",
+	/** The directory the output file should be relative to */
+	OutputDirectory = "od"
+}
+export const CompilerOptions: Record<Options, string> = {
+	[Options.OutputFile]: "./out",
+	[Options.OutputDirectory]: "./",
+}
+export enum Flags {
+	/** If true, debug information will be printed */
+	Debug = "d"
+}
+export const CompilerFlags: Record<Flags, boolean> = {
+	[Flags.Debug]: false,
+}
+
+for (let argumentIndex = 0; argumentIndex < CompilerArguments.length; argumentIndex++) {
+	if (CompilerArguments[argumentIndex][0] == "-") {
+		//Handle options
+		const name = CompilerArguments[argumentIndex].substring(1)
+		if (name in CompilerFlags) {
+			CompilerFlags[name as Flags] = true
+		}
+		else if (name in CompilerOptions) {
+			CompilerOptions[name as Options] = CompilerArguments[++argumentIndex]
+		}
+	}
+	else {
+		Inputs.push(CompilerArguments[argumentIndex])
+	}
+}
+
+/** The first input, assumed to be the source file */
 export const SourceFile = Inputs[0]
 
-// The location of the compiler source files
-export const CompilerDirectory = path.resolve(__dirname, "../ts")
+/** The location of the compiler source directory */
+export const CompilerRootDirectory = path.resolve(__dirname, "../")
