aa834e7dd2ee546775fffa70fdfe3f05764d60db
Marcel Mukundi <45983298+MMukundi@users.noreply.github.com>
2021-12-07 21:48:25 -0800

    Declared loggers to be used in the compiler

---
+++ bootstrap/ts/loggers.ts
@@ -0,0 +1,10 @@
+import { CompilerArguments, CompilerFlags, Flags } from "./arguments"
+import { BufferedStyledLogger, ConsoleColor } from "./style"
+
+export const stdWrite = process.stdout.write.bind(process.stdout)
+
+export const errorLogger = new BufferedStyledLogger(stdWrite, "toast {error}: ", "\n", ConsoleColor.Red)
+export const warningLogger = new BufferedStyledLogger(stdWrite, "toast {warn}: ", "\n", ConsoleColor.Yellow)
+export const noteLogger = new BufferedStyledLogger(stdWrite, "toast {note}: ", "\n", ConsoleColor.Blue)
+export const debugLogger = new BufferedStyledLogger((buffer) => CompilerFlags[Flags.Debug] ? stdWrite(buffer) : null, "toast {debug}: ", "\n", ConsoleColor.Purple)
+
